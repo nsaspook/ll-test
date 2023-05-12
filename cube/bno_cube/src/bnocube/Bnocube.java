6dcd4ae2aacce66e8a17f23a49729d9fa4cbeca4
nsaspook <nsaspook@nsaspook.com>
2023-05-11 20:29:25 -0700

    up

--- cube/bno_cube/src/bnocube/Bnocube.java
+++ cube/bno_cube/src/bnocube/Bnocube.java
@@ -2,9 +2,13 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *
+ * http://www.java2s.com/example/java-api/javax/media/j3d/orientedshape3d/rotate_about_point-0.html
+ * send an ascii string of CSV data values via a tcp port
  */
 package bnocube;
 
+import java.awt.*;
 import java.util.Scanner;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
@@ -13,12 +17,15 @@ import javax.media.j3d.TransformGroup;
 import javax.swing.JFrame;
 import javax.vecmath.Quat4d;
 import javax.vecmath.Vector3d;
+import javax.vecmath.Point3f;
 import com.fazecast.jSerialComm.SerialPort;
 import com.sun.j3d.utils.geometry.ColorCube;
+import javax.media.j3d.Text3D;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 import java.util.Properties;
 import java.net.Socket;
 import java.net.InetAddress;
+import javax.media.j3d.*;
 
 /**
  *
@@ -34,9 +41,15 @@ public class Bnocube {
         SimpleUniverse universe = new SimpleUniverse(canvas);
         BranchGroup group = new BranchGroup();
         ColorCube cube = new ColorCube(0.3);
+        Font myFont = new Font("TimesRoman", Font.CENTER_BASELINE, 1);
+        Font3D myFont3D = new Font3D(myFont, new FontExtrusion());
+        Point3f textPt = new Point3f(-1.6f, 0.4f, 0.0f);
+        Text3D myText3D = new Text3D(myFont3D, "BNO086", textPt);
+        Shape3D myShape3D = new Shape3D(myText3D, new Appearance());
 
         TransformGroup transformGroup = new TransformGroup();
         transformGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        transformGroup.addChild(myShape3D);
         transformGroup.addChild(cube);
 
         universe.getViewingPlatform().setNominalViewingTransform();
@@ -68,19 +81,16 @@ public class Bnocube {
                         System.out.println(String.format("dtype = %3s  device = %s :%s:%s:%s:%s:%s:%s:%s:", token[0], token[1], token[2], token[3], token[4], token[5], token[6], token[7], token[8]));
                         // multiply x/y/z by -1 to swap frames of reference
                         double x = Double.parseDouble(token[4]);
-                        double y = -Double.parseDouble(token[3]);
-                        double z = Double.parseDouble(token[2]);
+                        double y = Double.parseDouble(token[2]);
+                        double z = -Double.parseDouble(token[3]);
                         double w = Double.parseDouble(token[5]);
                         double ax = Double.parseDouble(token[6]);
                         double ay = Double.parseDouble(token[7]);
                         double az = Double.parseDouble(token[8]);
-                        /*
-                System.out.println(String.format("w = %+2.3f     x = %+2.3f     y = %+2.3f     z = %+2.3f", w, x, y, z));
-                         */
 
                         Quat4d quaternion = new Quat4d(w, x, y, z);
                         Vector3d vector = new Vector3d((az * 0.02), (ay * 0.02), (az * 0.02));
-                        transformGroup.setTransform(new Transform3D(quaternion, vector, 1.0));
+                        transformGroup.setTransform(new Transform3D(quaternion, vector, 0.5));
 
                         // the inverse cosine of w gives you the pitch *if* you normalize the quaternion with x and z being zero
                         double pitch = Math.acos(w / Math.sqrt(w * w + y * y)) * 2.0 - (Math.PI / 2.0);
@@ -99,9 +109,7 @@ public class Bnocube {
             System.err.println("Unable to open socket to ttl_eth_host. Exiting.");
             System.exit(1);
         }
-        System.err.println("Lost communication with the serial port. Exiting.");
+        System.err.println("Exiting.");
         System.exit(1);
-
     }
-
 }
--- ll-tester.pro
+++ ll-tester.pro
@@ -2,24 +2,24 @@ EAGLE AutoRouter Statistics:
 
 Job           : /root/ll-test/ll-tester.brd
 
-Start at      : 12:34:19 (5/10/23)
-End at        : 12:39:17 (5/10/23)
-Elapsed time  : 00:04:56
+Start at      : 05:34:17 (5/11/23)
+End at        : 06:30:31 (5/11/23)
+Elapsed time  : 00:56:12
 
 Signals       :    89   RoutingGrid: 3 mil  Layers: 4
-Connections   :   355   predefined:  353 ( 130 Vias )
+Connections   :   355   predefined:  25 ( 0 Vias )
 
 Router memory :   26051136
 
 Passname          : TopRouter     Route Optimize1 Optimize2 Optimize3 Optimize4
 
-Time per pass     :  00:04:34  00:00:01  00:00:05  00:00:06  00:00:05  00:00:05
-Number of Ripups  :         0         0         0         0         0         0
-max. Level        :         0         0         0         0         0         0
-max. Total        :         0         0         0         0         0         0
+Time per pass     :  00:03:17  00:34:31  00:05:10  00:04:20  00:04:30  00:04:24
+Number of Ripups  :         0       101         0         0         0         0
+max. Level        :         0         3         0         0         0         0
+max. Total        :         0       147         0         0         0         0
 
-Routed            :         0         2         2         2         2         2
-Vias              :         0         0         0         0         0         0
-Resolution        :    99.4 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %
+Routed            :        86       330       328       330       330       330
+Vias              :         0       148       156       148       140       141
+Resolution        :    31.3 %   100.0 %    99.4 %   100.0 %   100.0 %   100.0 %
 
 Final             : 100.0% finished
